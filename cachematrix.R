d0fb9fe87974e756134ca245c6321cf592da3f7d
BeRad-Moondog <96245404+BeRad-Moondog@users.noreply.github.com>
2022-01-03 19:28:39 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        # x is a square invertible matrix
+        #This function returns a list of functions for setting and getting the special matrix m.
+        #Also the returned list provides functions for getting and setting the inverse of the matrix m
+        #The returned list is used as an input into cacheSolve(x)
+        
+        inv = NULL
+        set = function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get = function() x
+        setinv = function(inverse) inv <<- inverse 
+        getinv = function() inv
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # x is the output of makeCacheMatrix()
+        #This function returns the inverse of the input special matrix created by makeCacheMatrix()
+        inv = x$getinv()
+        
+        if (!is.null(inv)){
+                return(inv)  #skip calculation of the inverse it if has already been calculated
+        }else{
+                mat.data = x$get()
+                inv = solve(mat.data, ...)
+                 x$setinv(inv)
+        }
+        
+        return(inv)
 }
+
+##Test Code:
+## x<-matrix(rnorm(16),4,4)
+## cacheSolve(makeCacheMatrix(x)) %*% x     ###Should provide a resonable approximation of the Identity Matrix
